7333d3badc35a02bf82b9d78c514e72743849b01
zhuyanhao <yhzhu@umich.edu>
2019-09-27 17:58:59 -0400

    Add support to define variable of interest in forward integration. Need to complete the backward integration.

--- Ode1stExplicit.py
+++ Ode1stExplicit.py
@@ -26,6 +26,8 @@ class Ode1stExplicit(ABC):
     def __init__ (self, dim_y, dim_p, dim_z, method="RK45"):
         self.dim_y = dim_y
         self.dim_p = dim_p
+        self.dim_z = dim_z
+        self.dzdp = np.zeros((dim_z, dim_p))
         self.method = method
 
     ## Method provided by user ################################################
@@ -44,30 +46,143 @@ class Ode1stExplicit(ABC):
         pass
 
     @abstractmethod
-    def I1(self, t, y, p):
+    def fp(self, t, y):
+        """
+        Partial derivative of f w.r.t parameters p.
+        fp[i][j] - derivative of f_i w.r.t p_j
+        """
+        pass
+
+    @abstractmethod
+    def I1(self, t, y):
         """
         Value of I1. Row vector with d = dim_z
         """
+        pass
+
+    @abstractmethod
+    def dI1dp(self, t, y):
+        """
+        Derivative of I1 w.r.t parameters p. 
+        dI1dp[i][j] - derivative of I1_i w.r.t p_j
+        """
+        pass
+
+    @ abstractmethod
+    def dI1dy(self, t, y):
+        """
+        Derivative of I1 w.r.t states y.
+        dI1dy[i][j] - derivative of I1_i w.r.t y_j
+        """
+        pass
+
+    @abstractmethod
+    def I2(self, t, y):
+        """
+        Value of I2. Row vector with d = dim_z
+        """
+        pass
+
+    @abstractmethod
+    def dI2dp(self, t, y):
+        """
+        Derivative of I2 w.r.t parameters p. 
+        dI2dp[i][j] - derivative of I2_i w.r.t p_j
+        """
+        pass
+
+    @ abstractmethod
+    def dI2dy(self, t, y):
+        """
+        Derivative of I1 w.r.t states y.
+        dI2dy[i][j] - derivative of I2_i w.r.t y_j
+        """
+        pass
+
+    ## Method used by Integrator ##############################################
+    def rhs(self, t, y_aux):
+        """
+        The right hand side of the entire 1st system. y_aux = [y, z]
+        """
+        y = y_aux[:self.dim_y]
+        y_aux_dot = np.zeros(self.dim_y+self.dim_z)
+        y_aux_dot[:self.dim_y] = self.f(t, y)
+        y_aux_dot[self.dim_y:] = self.I2(t,y)
+        return y_aux_dot
+
+    def rhs_adjoint(self, t, y_aux):
+        """
+        RHS of adjoint system. y_aux = [y, z, lambda, dz_i/dp_j]
+        """
+        dim_l = self.dim_z * self.dim_y
+        dim_zp = self.dim_z * self.dim_p
+        y_aux_dot = np.zeros(self.dim_y + self.dim_z + dim_l + dim_zp)
+        
+        # Getting value from auxilary states
+        y = y_aux[0:self.dim_y]
+        z = y_aux[self.dim_y:self.dim_y+self.dim_z]
+        # l = y_aux[self.dim_y+self.dim_z:self.dim_y+self.dim_z+dim_l]
+        
+        # Compute y_dot
+        y_aux_dot[0:self.dim_y] = self.f(t,y)
+        # Compute z_dot
+        y_aux_dot[self.dim_y:self.dim_y+self.dim_z] = self.I2(t, y)
+        # Compute lambda_dot
+        l_index = self.dim_y+self.dim_z
+        dh_dx_t = -self.fj(t, y).T
+        dI2dy = self.dI2dy(t, y)
+        for i in range(self.dim_z):
+            l_i = y_aux[l_index:l_index+self.dim_y].reshape(self.dim_y, 1)
+            l_i_dot = dI2dy[i,:] + dh_dx_t.dot(l_i)
+            y_aux_dot[l_index:l_index+self.dim_y] = l_i_dot
+            l_index += self.dim_y
+        # Compute dz_i/dp_j_dot
+        l_index = self.dim_y+self.dim_z
+        zp_index = self.dim_y + self.dim_z + dim_l
+        dI2dp = self.dI2dp(t, y)
+        dhdp = -self.fp(t, y)
+        for i in range(self.dim_z):
+            l_i = y_aux[l_index:l_index+self.dim_y].reshape(1, self.dim_y)
+            dzdp_i = dI2dp[i].reshape(self.dim_p, 1) + l_i.dot(dhdp)
+            y_aux_dot[zp_index:zp_index+self.dim_p] = dzdp_i
+            l_index += self.dim_y
+            zp_index += self.dim_p
+        
+        return y_aux_dot
 
     ## Forward Integration ####################################################
     def computeValue(self, t0, t1, y0):
         """
         Forward integration to compute the value of states
         """
-        self.y = y0
+        assert len(y0) == self.dim_y
+        y_aux = np.zeros(self.dim_y+self.dim_z)
+        y_aux[:self.dim_y] = y0
 
         sol = solve_ivp(
-            fun = self.f,
+            fun = self.rhs,
             t_span = (t0, t1),
-            y0 = self.y,
+            y0 = y_aux,
             method = self.method,
-            vectorized=False,
-            jac = self.fj,
+            vectorized = False,
             dense_output = True,
             )
 
-        self.y = sol.y[:,-1]   # Get the last column
+        if not sol.success:
+            raise RuntimeError("Integration Fails.")
+
+        y_aux = sol.y[:,-1]   # Get the last column
+        self.y = y_aux[:self.dim_y]
+        self.z = y_aux[self.dim_y:] + self.I1(t1, self.y)
         return sol
+
+    ## Backward Integration ###################################################
+    def computeSensitivity(self, t0, t1, y0):
+        """
+        Backward integration to compute the value of sensitivity
+        """
+        self.computeValue(t0, t1, y0)  # Compute self.y = y_1
+
 
     ## Property ###############################################################
     @property
@@ -135,4 +250,28 @@ class Ode1stExplicit(ABC):
         else:
             raise RuntimeError("Dimension of z doesn't match. Cannot set z to new value.")
 
+    # Adjoint variable
+    @property
+    def l(self):
+        return self._l
+    
+    @l.setter
+    def l(self, value):
+        new_l = np.array(value)
+        if new_l.shape == self._l.shape:
+            self._l = new_l
+        else:
+            raise RuntimeError("Dimension of l doesn't match. Cannot set z to new value.")
+
+    @property
+    def dzdp(self):
+        return self._dzdp
+    
+    @dzdp.setter
+    def dzdp(self, value):
+        new_dzdp = np.array(value)
+        if new_dzdp.shape == (self.dim_z, self.dim_p):
+            self._dzdp = new_dzdp
+        else:
+            raise RuntimeError("Dimension of l doesn't match. Cannot set z to new value.")
     
--- test/test_simpleClosedForm.py
+++ test/test_simpleClosedForm.py
@@ -3,22 +3,70 @@ import numpy as np
 
 class SimpleClosedForm(Ode1stExplicit):
     """
-    dx_dt = p[0]x + p[1]
+    dy_dt = p[0]y
+    z[0] = y1 + p[0]
+    z[1] = p[1] + int_t(y)
     """
     def f(self, t, y):
-        return self.p[0]*y[0]
+        return np.array([self.p[0]*y[0]])
     
     def fj(self, t, y):
         return [[self.p[0]]]   # Shape (1,1)
+    
+    def fp(self, t, y):
+        pass
+
+    def I1(self,t,y):
+        ans = np.zeros(2)
+        ans[0] = y[0] + self.p[0]
+        ans[1] = self.p[1]
+        return ans
+    
+    def I2(self,t,y):
+        ans = np.zeros(2)
+        ans[1] = y[0]
+        return ans
+
+    def dI1dp(self, t, y):
+        """
+        Derivative of I1 w.r.t parameters p. 
+        dI1dp[i][j] - derivative of I1_i w.r.t p_j
+        """
+        pass
+
+    def dI1dy(self, t, y):
+        """
+        Derivative of I1 w.r.t states y.
+        dI1dy[i][j] - derivative of I1_i w.r.t y_j
+        """
+        pass
+
+    def dI2dp(self, t, y):
+        """
+        Derivative of I2 w.r.t parameters p. 
+        dI2dp[i][j] - derivative of I2_i w.r.t p_j
+        """
+        pass
+
+    def dI2dy(self, t, y):
+        """
+        Derivative of I1 w.r.t states y.
+        dI2dy[i][j] - derivative of I2_i w.r.t y_j
+        """
+        pass
 
 def test_RK45():
-    test = SimpleClosedForm(1, 1, method="RK45")
-    test.p = [2]
+    test = SimpleClosedForm(1, 2, 2, method="RK45")
+    test.p = [2, 3]
     test.computeValue(0, 1, [1])
     assert np.isclose(test.y[0], np.e**2, 1e-2, 1e-2)
+    assert np.isclose(test.z[0], np.e**2+2, 1e-2, 1e-2)
+    assert np.isclose(test.z[1], 0.5*np.e**2-0.5+3, 1e-2, 1e-2)
 
 def test_BDF():
-    test = SimpleClosedForm(1, 1, method="BDF")
-    test.p = [2]
+    test = SimpleClosedForm(1, 2, 2, method="BDF")
+    test.p = [2, 3]
     test.computeValue(0, 1, [1])
     assert np.isclose(test.y[0], np.e**2, 1e-2, 1e-2)
+    assert np.isclose(test.z[0], np.e**2+2, 1e-2, 1e-2)
+    assert np.isclose(test.z[1], 0.5*np.e**2-0.5+3, 1e-2, 1e-2)
